b3b0b460a2f41db7f21ab38cfca8ab9f757dcf3c
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2022-11-16 18:06:15 +0000

    Converter
    Converter conteúdo para inteiro, decimal, character.

---
+++ Convert.py
@@ -0,0 +1,10 @@
+#Converter para inteiro
+x = int(10.1) #Resultado = 10
+y = int('10') #Resultado = 10
+
+#Converter para decimal
+x = float(10) #Resultado = 10.0
+y = float('10') #Resultado = 10.0
+
+#Converter para character
+x = str(10) #Resultado = 10
