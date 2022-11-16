664206a518958acbf3e18261136c9540051dc48a
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2022-11-16 18:26:32 +0000

    String métodos
    String métodos

---
+++ String.py
@@ -0,0 +1,6 @@
+a="Teste de codigo"
+print(a.lower()) #Resultado: teste de codigo
+print(a.upper()) #Resultado: TESTE DE CODIGO
+print(a.capitalize()) #Resultado: Teste de codigo
+print(a.replace('e','a')) #Resultado: Tasta da codigo
+print(a.find('de')) #Resultado: 6
