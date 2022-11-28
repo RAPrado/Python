6a1495f5e52601e490a5ce1b748f3766a424d1e9
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2022-11-28 17:00:05 +0000

    Dicionário : chave e valor
    Dicionário : chave e valor

--- Dicionarios.py
+++ Dicionarios.py
@@ -1,4 +1,4 @@
-#Referencia : https://openclassrooms.com/en/courses/2304731-learn-python-basics-for-data-analysis/7979496-create-collections-to-store-your-objects
+#Referência : https://openclassrooms.com
 
 #Cria um dicionário com chave e valor
 accounts = {'Marion Weaver': 10000, 'Alberto Mendoza': 150, 'Katharine Tyler': 300, 'Isaac Steele': 1800.74}
