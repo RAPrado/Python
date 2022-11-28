c3f5e09137c82aa68fdb977d1498b189db486816
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2022-11-28 16:34:10 +0000

    Tuplas
    Tuplas

---
+++ Tupla.py
@@ -0,0 +1,17 @@
+#Referencia : https://openclassrooms.com/en/courses/2304731-learn-python-basics-for-data-analysis/7979496-create-collections-to-store-your-objects
+
+#Dupla são listas que não podem ser modificadas. São declaradas usando parenteses e não chaves (contrário das listas puras).
+my_tuple = (1, 2, 3, 'a', 'b')
+
+#Imprime o conteúdo
+print(my_tuple[1]) # -> 2
+print(my_tuple[4]) # -> 'b'
+
+#Tenta atribuir um valor, mas dá erro.
+my_tuple[1]=2
+
+#---------------------------------------------------------------------------
+#TypeError                                 Traceback (most recent call last)
+#<ipython-input-7-61754d14ee68> in <module>
+#----> 1 my_tuple[1]=2
+#TypeError: 'tuple' object does not support item assignment
