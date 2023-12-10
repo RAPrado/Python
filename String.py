e14d3943f2ac54cb88982128d24d70cc27e19ba2
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2023-12-10 17:48:25 +0000

    Strings methods

--- String.py
+++ String.py
@@ -1,6 +1,20 @@
+#Example 1
 a="Teste de codigo"
 print(a.lower()) #Resultado: teste de codigo
 print(a.upper()) #Resultado: TESTE DE CODIGO
 print(a.capitalize()) #Resultado: Teste de codigo
 print(a.replace('e','a')) #Resultado: Tasta da codigo
 print(a.find('de')) #Resultado: 6
+
+
+#Example 2 - Getting specifics position in a string
+data="10/dezembro/2023"
+
+posicao=data.find('/')
+dia = data[0:2]           #Get 2 left positions
+mes = data[posicao+1:-5]  #Get the month, starting after de bar position, minus the last 5 positions
+ano = data[-4:]           #Get 4 right positions
+
+print(dia) #Resulsato : 10
+print(mes) #Resulsato : dezembro
+print(ano) #2023
