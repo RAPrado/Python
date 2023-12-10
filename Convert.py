f46cef0c63b9cff7e83bec33e6a1c47dff9466ec
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2023-12-10 21:04:05 +0000

    Coverting Number, character, date

--- Convert.py
+++ Convert.py
@@ -8,3 +8,19 @@ y = float('10') #Resultado = 10.0
 
 #Converter para character
 x = str(10) #Resultado = 10
+
+#Coverter para data
+from datetime import datetime
+dia = str(1)
+mes = str(12)
+ano = str(2023)
+
+#Exemplo 1
+data = dia+'/'+mes+'/'+ano #1/12/2023
+data_convertida =  datetime.strptime(data,/ '%d/%m/%Y').date()
+print(data_convertida) #01/12/2023
+
+#Exemplo 2
+data = ano+'/'+mes+'/'+dia #2023/12/1
+data_convertida = datetime.strptime(str_date, '%Y-%m-%d').date()
+print(data_convertida) #2023/12/01
