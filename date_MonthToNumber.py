0df4ea50cca39f6270a52c4473b67240f5fb5f06
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2023-12-10 18:51:03 +0000

    #Receives the name of the month literally and returns it as a number

---
+++ date_MonthToNumber.py
@@ -0,0 +1,16 @@
+#Receives the name of the month literally and returns it as a number
+def mesParaNumero(mes:str):
+    return {
+            'janeiro': 1,
+            'fevereiro': 2,
+            'março': 3,
+            'abril': 4,
+            'maio': 5,
+            'junho': 6,
+            'julho': 7,
+            'agosto': 8,
+            'setembro': 9, 
+            'outubro': 10,
+            'novembro': 11,
+            'dezembro': 12
+    }[mes.lower()]
