0dc43cb817d7846cb947a30ce34ba8061e742256
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2024-02-29 16:36:34 +0000

    Create or open a file

---
+++ file_open_create.py
@@ -0,0 +1,13 @@
+#Reference : https://www.pythontutorial.net/python-basics/python-create-text-file/
+
+#Create a text file
+f = open ('file_name.txt,'w',encoding='utf-8')
+
+#where
+#'w' – open a file for writing, and if the file doesn’t exist, the open() function creates a new file. Otherwise, it’ll overwrite the contents of the existing file.
+#'x' – open a file for exclusive creation, and if the file exists, the open() function raises an error (FileExistsError). Otherwise, it’ll create the text file.
+
+#Record the information in file
+f.write('Infomation' +  '\n') #Insert a break line
+
+f.close() #Close the file
