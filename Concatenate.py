191a4e4fea36ec6ef51f5c9fbf21bccbcdfbc432
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2024-02-29 14:00:52 +0000

    Concatenating string, value or list.

--- Concatenate.py
+++ Concatenate.py
@@ -3,3 +3,8 @@ x = 'Parte a' + 'Parte b'
 
 #Concatenando palavra e número
 x = 'Parte a' + str(10)
+
+#Concatenando com separador
+lista = ['Campo1', 'Campo2', 'Campo3']
+conteudo = '|'.join(lista)
+print(conteudo) #Campo1|Campo2|Campo1
