ba8f464195143a8028753bc5ca8ccebeebc75aea
Rodrigo Prado <83780527+RAPrado@users.noreply.github.com>
2024-04-29 22:16:13 +0100

    Exemplos usando print

---
+++ Print.py
@@ -0,0 +1,11 @@
+#Exemplos usando print
+valor = 1
+
+print("O valor é:",valor)
+#O valor é: 1
+
+print(f"O valor é:{valor}")
+#O valor é:1
+
+print("O valor é inferior a 5" if valor < 5 else "O valor é >= a 5")
+#O valor é inferior a 5
